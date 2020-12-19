1a81a1ea04dc44c4e7b36c456bcf34a1f9fca9b5
Antop <antop@naver.com>
2020-12-19 20:21:45 +0900

    pro-spring-boot-5 스프링 부트 웹 개발

--- spring-boot-journal/src/main/kotlin/com/apress/spring/repository/JournalRepository.kt
+++ spring-boot-journal/src/main/kotlin/com/apress/spring/repository/JournalRepository.kt
@@ -1,6 +1,23 @@
 package com.apress.spring.repository
 
-import com.apress.spring.domain.Journal
+import com.apress.spring.domain.JournalEntry
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.repository.query.Param
+import org.springframework.data.rest.core.annotation.RepositoryRestResource
+import org.springframework.format.annotation.DateTimeFormat
+import java.time.LocalDate
 
-interface JournalRepository : JpaRepository<Journal, Long>
+@RepositoryRestResource(collectionResourceRel = "entry", path = "journal")
+interface JournalRepository : JpaRepository<JournalEntry, Long> {
+
+    fun findByCreatedAfter(@Param("after") @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) date: LocalDate): List<JournalEntry>
+
+    fun findByCreatedBetween(
+        @Param("after") @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) after: LocalDate,
+        @Param("before") @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) before: LocalDate
+    ): List<JournalEntry>
+
+    fun findByTitleContaining(@Param("word") word: String): List<JournalEntry>
+
+    fun findBySummaryContaining(@Param("word") word: String): List<JournalEntry>
+}
